2d701c27af0cd48bfd5ba7c87433474a45d19a2f
Dmytro Krakhmalov <dmitrykup1337@gmail.com>
2023-02-07 22:19:10 +0200

    Homework 16

--- Homework 16.js
+++ Homework 16.js
@@ -1,4 +1,7 @@
 "use strict"
+//Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length.
+// Додаю до змінної result рандомний символ(через його індекс СharAt) з нашої стрінги з набором символів за допомогою простої математичної функції генерування рандомного числа, яке за допомогою ParseInt роблю цілим, і яке генерується в діапазоні довжини нашої стрінги сharacters;
+
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 function generateKey(lenght, characters) {
     let result = "";
