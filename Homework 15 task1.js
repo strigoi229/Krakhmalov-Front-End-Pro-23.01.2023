a45dd25ffe0ddc98d324f6735053b837f9150d13
Dmytro Krakhmalov <dmitrykup1337@gmail.com>
2023-02-08 19:31:34 +0200

    Homework 15

---
+++ Homework 15 task1.js
@@ -0,0 +1,41 @@
+"use strict"
+//Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
+function min(a, b) {
+    if (a < b) {
+        return console.log(a);
+    } else console.log(b);
+}
+min(5, 100);
+
+//Напишіть функцію pow(x, n), яка повертає число x в степені n
+function pow(x, n) {
+    return console.log(x ** n);
+}
+pow(2, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Homework 15 task2.js
@@ -0,0 +1,48 @@
+"use strict"
+// ?
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Батьки дозволили?');
+}
+checkAge(17);
+
+// "||"
+// function checkAge(age) {
+//     return (age > 18) || confirm('Батьки дозволили?');
+// }
+// checkAge(19);
+
+// "&&"
+// function checkAge(age) {
+//     return !(age > 18) && (confirm('Батьки дозволили?'));
+// }
+// checkAge(19);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
